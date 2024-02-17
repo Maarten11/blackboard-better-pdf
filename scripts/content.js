88b7bb3b713ce740426db1e4bfbcf73a0fdfd4c4
Maarten <maarten.peirsman@gmail.com>
2024-02-17 22:22:43 +0100

    Basic working version

--- scripts/content.js
+++ scripts/content.js
@@ -1,63 +1,7 @@
-// const article = document.querySelector("article");
-// // `document.querySelector` may return null if the selector doesn't match anything.
-// if (article) {
-// 	const text = article.textContent;
-// 	const wordMatchRegExp = /[^\s]+/g; // Regular expression
-// 	const words = text.matchAll(wordMatchRegExp);
-// 	// matchAll returns an iterator, convert to array to get word count
-// 	const wordCount = [...words].length;
-// 	const readingTime = Math.round(wordCount / 200);
-// 	const badge = document.createElement("p");
-// 	// Use the same styling as the publish information in an article's header
-// 	badge.classList.add("color-secondary-text", "type--caption");
-// 	badge.textContent = `⏱️ ${readingTime} min read`;
-// 	// Support for API reference docs
-// 	const heading = article.querySelector("h1");
-// 	// Support for article docs with date
-// 	const date = article.querySelector("time")?.parentNode;
-// 	(date ?? heading).insertAdjacentElement("afterend", badge);
-// }
-chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-    console.log(message);
-    return true;
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+	if (request.message === "get-pdfs") {
+		const previews = document.querySelectorAll("bb-file-preview");
+		console.log(previews);
+		document.body.style.backgroundColor = "blue";
+	}
 });
-function returnIFrameChild(element) {
-    if (!element.hasChildNodes)
-        return null;
-    var queue = Array.from(element.children);
-    queue.forEach(function (child) {
-        console.log(child, child.tagName);
-        if (child.tagName === "IFRAME") {
-            return child;
-        }
-        else {
-            if (child.hasChildNodes()) {
-                Array.from(child.children).forEach(function (c) { return queue.push(c); });
-            }
-        }
-    });
-    return null;
-}
-var previews = document.querySelectorAll("bb-file-preview");
-if (!!previews.length) {
-    previews.forEach(function (p) {
-        p.style.color = "red";
-    });
-    // const list = document.createElement("ul");
-    // previews.forEach((p) => {
-    // 	p.style.color = "red !important";
-    // 	const iFrame = returnIFrameChild(p);
-    // 	if (!iFrame) return;
-    // 	const item = document.createElement("li");
-    // 	item.innerText = iFrame.getAttribute("src")!;
-    // 	list.appendChild(item);
-    // });
-    // previews[0].parentNode!.insertBefore(previews[0], list);
-}
-document.onreadystatechange = function () {
-    if (document.readyState === "complete") {
-        previews.forEach(function (p) {
-            p.style.color = "blue";
-        });
-    }
-};
--- scripts/popup.js
+++ scripts/popup.js
@@ -1,10 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
-	const button = document.getElementById("get-links");
-
-	button.addEventListener("click", function () {
-		button.style.backgroundColor = "red";
-		chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-			chrome.tabs.sendMessage(tabs[0].id, { action: "collect" });
+	document.getElementById("get-links").addEventListener("click", () => {
+		chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+			chrome.tabs.sendMessage(tabs[0].id, { message: "get-pdfs" });
 		});
 	});
 });
